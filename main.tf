e3d60dbd6d379c7edf696bdf05ecb08a33411266
Mário Serrador <36656366+mruiserr@users.noreply.github.com>
2023-10-03 12:04:49 +0200

    Update main.tf
    Snyk fix

--- main.tf
+++ main.tf
@@ -4,12 +4,10 @@ provider "aws" {
   skip_credentials_validation = true
   skip_requesting_account_id  = true
   skip_metadata_api_check     = true
-  access_key                  = "mock_access_key"
-  secret_key                  = "mock_secret_key"
 }
 
 resource "aws_iam_account_password_policy" "strict" {
-  minimum_password_length = 8
+  minimum_password_length = 14
   #require_lowercase_characters   = true
   #require_numbers                = true
   #require_uppercase_characters   = true
